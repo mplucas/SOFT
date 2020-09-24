8a4c873155780162297d704f5d5cdb780691b75c
Lucas Meneghelli Pereira <lucas-m.p@hotmail.com>
2020-09-23 23:54:39 -0300

    - Removed pgadmin dependence - Removed possibility of updating document of student

--- src/main/java/com/example/myapp/MyappApplication.java
+++ src/main/java/com/example/myapp/MyappApplication.java
@@ -3,17 +3,16 @@ package com.example.myapp;
 import java.sql.SQLException;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 @SpringBootApplication
 public class MyappApplication {
 
 	public static void main(String[] args) throws SQLException {
 		SpringApplication.run(MyappApplication.class, args);
-		
-		//test
-//		UserDAOImpl userDAO = new UserDAOImpl();
-//		userDAO.saveUser(22, "teste2");
 	}
-
 }
